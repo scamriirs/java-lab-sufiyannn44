9774ee5be385be421363137b33a54a96b6a85837
Sufiyan Ali <155474207+sufiyannn44@users.noreply.github.com>
2025-09-26 17:09:13 +0530

    Add files via upload

---
+++ defaultconst.java
@@ -0,0 +1,9 @@
+public class defaultconst {
+    
+    defaultconst(){
+        System.out.print("hello");
+    }
+    public static void main(String[]args){
+        defaultconst f = new defaultconst();
+    }
+}
