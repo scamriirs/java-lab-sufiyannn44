eeb3a0608c02d6980086fcf6e51b6bbae56d8c85
Sufiyan Ali <155474207+sufiyannn44@users.noreply.github.com>
2025-11-19 21:55:35 +0530

    Delete defaultconst.java

--- defaultconst.java
+++
@@ -1,9 +0,0 @@
-public class defaultconst {
-    
-    defaultconst(){
-        System.out.print("hello");
-    }
-    public static void main(String[]args){
-        defaultconst f = new defaultconst();
-    }
-}
